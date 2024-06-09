8a10e9c43d27e74c9c1ee58722d48b6eecf4d9f7
Vidur Ratna <vidur_ratna@me.com>
2024-06-09 13:18:34 +1200

    remove useless code

--- app/page.tsx
+++ app/page.tsx
@@ -47,26 +47,3 @@ export default function Home() {
     </main>
   );
 }
-
-{/* <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="flex justify-center flex-col text-center gap-4">
-        <h1 className="text-4xl font-bold">ISSO Hamilton</h1>
-        <p className="max-w-screen-sm">Do you want to donate once off for an event or repeatedly donate towards a seva? </p>
-      </div>
-      <div className="flex justify-center gap-4 pt-12">
-        <Link 
-          href={"/once"}
-        >
-          <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Donate Once Off
-          </div>
-        </Link>
-        <Link 
-          href={"/regular"}
-        >
-          <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Regular Donation
-          </div>
-        </Link>
-      </div>
-    </main> */}
