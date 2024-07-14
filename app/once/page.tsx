2a51fe0620b2c4e7f13e3ed1a2ee5d45dffccc7d
Jayan Ratna <jayanratna@gmail.com>
2024-07-14 15:38:23 +1200

    small fixes

--- app/once/page.tsx
+++ app/once/page.tsx
@@ -24,13 +24,17 @@ export default function Home() {
         <div className="bg-white p-8 md:p-16 rounded-md mx-4 md:ml-8 lg:ml-20 shadow-md flex flex-col items-start">
           <Link 
             href={"/"}
+            className="flex space-x-2 items-center mb-4 bg-blue-100 hover:bg-blue-300 text-blue-900 font-semibold py-2 px-4 rounded-lg"
           >
-            <div className=" mb-4 bg-blue-100 hover:bg-blue-300 text-black font-bold py-2 px-4 rounded">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 opacity-50">
+              <path fillRule="evenodd" d="M18 10a.75.75 0 0 1-.75.75H4.66l2.1 1.95a.75.75 0 1 1-1.02 1.1l-3.5-3.25a.75.75 0 0 1 0-1.1l3.5-3.25a.75.75 0 1 1 1.02 1.1l-2.1 1.95h12.59A.75.75 0 0 1 18 10Z" clipRule="evenodd" />
+            </svg>
+            <span>
               Go back home
-            </div>
+            </span>
           </Link>
-          <h1 className="text-3xl font-bold mb-2">One Off donation</h1>
-          <p className="max-w-[400px]">You can make a one off donation to a seva or an event, just fill out the reason for donation </p>
+          <h1 className="text-3xl font-bold mb-2 tracking-tight">One off donation</h1>
+          <p className="max-w-[400px]">You can make a one off donation to a seva or an event, fill out the reason for donation.</p>
           <div className="flex justify-center gap-4 pt-6">
           <Elements stripe={stripePromise}>
               <OncePaymentForm />
