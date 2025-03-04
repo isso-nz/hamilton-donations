5b5cf4f1e89704a80e6011fa6f857ccf5e9eb0e5
Jayan Ratna <jayanratna@gmail.com>
2025-03-04 16:18:08 +1300

    fix: build issues with Tailwind and Stripe

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, VariantProps } from 'cva'
 import { cn } from '@/utils/ui'
 
 const buttonVariants = cva({
-  base: 'relative isolate inline-flex select-none items-center justify-center gap-x-2 rounded-lg border border-transparent px-3.5 py-2.5 text-base/6 font-semibold transition focus:outline-none disabled:pointer-events-none disabled:opacity-50 sm:px-3 sm:py-1.5 sm:text-sm/6',
+  base: 'relative isolate inline-flex select-none items-center justify-center gap-x-2 rounded-lg border border-transparent px-3.5 py-2.5 text-base/6 font-semibold transition focus:outline-hidden disabled:pointer-events-none disabled:opacity-50 sm:px-3 sm:py-1.5 sm:text-sm/6',
   variants: {
     variant: {
       primary: 'bg-orange-400 text-orange-950 hover:bg-orange-500',
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...pr
     <input
       type={type}
       className={cn(
-        'flex w-full rounded-lg border border-zinc-300 bg-white px-3.5 py-2.5 text-base/6 ring-offset-white transition file:border-0 focus-visible:border-orange-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-400/20 sm:px-3 sm:py-1.5 sm:text-sm/6',
+        'flex w-full rounded-lg border border-zinc-300 bg-white px-3.5 py-2.5 text-base/6 ring-offset-white transition file:border-0 focus-visible:border-orange-400 focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-orange-400/20 sm:px-3 sm:py-1.5 sm:text-sm/6',
         className
       )}
       ref={ref}
